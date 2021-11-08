4c4c2e9c56f9c0a59be327ed7cb8212fa762ebc7
Ben Evans <benjamin.john.evans@gmail.com>
2021-11-08 20:49:53 +0100

    Add JFRReader

---
+++ src/main/java/kathik/session3/JFRReader.java
@@ -0,0 +1,26 @@
+package kathik.session3;
+
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class JFRReader {
+    
+    public static void main(String[] args) {
+        var jfrFile = Path.of(args[0]);
+        try {
+            for (var event : RecordingFile.readAllEvents(jfrFile)) {
+                processEvent(event);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static void processEvent(RecordedEvent event) {
+
+    }
+}
